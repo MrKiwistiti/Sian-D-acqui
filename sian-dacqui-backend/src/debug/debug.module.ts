752da90b0620d61ac58080d884c54751edd75ab6
MrKiwistiti <sven5reichert@gmail.com>
2025-12-13 18:44:13 +0100

    fix: Register DebugController in module

--- sian-dacqui-backend/src/debug/debug.module.ts
+++ sian-dacqui-backend/src/debug/debug.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { DebugSchemaController } from './debug.schema.controller';
+import { DebugController } from './debug.controller';
 
 @Module({
-  controllers: [DebugSchemaController],
+  controllers: [DebugSchemaController, DebugController],
 })
 export class DebugModule {}
